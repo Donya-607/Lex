60d6d9d0546779999d3e022b75fe3d29d4c986a1
Donya <alias>
2020-03-15 18:33:30 +0900

    アニメーションを取得する際のサンプリングステップ計算処理を修正

--- Lex/source/ModelMotion.cpp
+++ Lex/source/ModelMotion.cpp
@@ -156,9 +156,8 @@ namespace Donya
 
 				const float diffL = currentSeconds    - keyFrameL.seconds;
 				const float diffR = keyFrameR.seconds - keyFrameL.seconds;
-				// const float percent = diffL / ( diffR + EPSILON/* Prevent zero-divide */ );
-				const float percent = ( currentSeconds - keyFrameL.seconds / keyFrameR.seconds - keyFrameL.seconds );
-
+				const float percent = diffL / ( diffR + EPSILON/* Prevent zero-divide */ );
+				
 				rv = Animation::KeyFrame::Interpolate( keyFrameL, keyFrameR, percent );
 
 				break;
